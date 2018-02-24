bc1cec700322832067838270f8b00a62f0076579
wimo7083 <wimo7083@colorado.edu>
2018-02-24 15:52:59 -0700

    more html scraping

--- htmlProcess/processMain.js
+++ htmlProcess/processMain.js
@@ -1,14 +1,38 @@
 // builds all tweet containers
 const getAllTweetDoms = () => {
   let users = [];
-  const allTweetElemnts = document.getElementsByClassName('js-stream-item');
+  const allTweetElements = document.getElementsByClassName('tweet');
   const builtTweetElements = allTweetElemnts.map(
     singleTweet => tweetObjectBuilder
   );
 };
 
-//
-const tweetObjectBuilder = singleTweet => {};
+// get url, get text, get tweet id, get user id
+const tweetObjectBuilder = singleTweet => {
+  const tweetInnerText = singleTweet.innerText;
+  const tweetId = singleTweet.getAttribute('data-item-id');
+  const userId = singleTweet.getAttribute('data-user-id');
+
+  // tweet container- assume one
+
+  // use data attribute for tweet id
+
+  let tweetObject = singleTweet.getElementsByClassName(
+    'js-tweet-text-container'
+  );
+};
+
+const singleLevelTweetText = jsTweetContainer => {
+  (child = jsTweetContainer.firstChild), (texts = []);
+
+  while (child) {
+    if (child.nodeType == 3) {
+      texts.push(child.data);
+    }
+    return child.nextSibling;
+  }
+  return texts.join('');
+};
 
 // returns if tweet is article or non-article
 const classifyTweetType = tweetDomItem => {
