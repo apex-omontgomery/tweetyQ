221f65cf9de895e25c249751aa7d3fb83efdba81
aidan <32107433+arafferty10@users.noreply.github.com>
2018-02-25 08:11:40 -0700

    edot

--- py_back/conceptsNLU.py
+++ py_back/conceptsNLU.py
@@ -4,11 +4,23 @@ from watson_developer_cloud import NaturalLanguageUnderstandingV1
 from watson_developer_cloud.natural_language_understanding_v1 \
   import Features, EntitiesOptions, KeywordsOptions, ConceptsOptions
 
+#Twitter Login Credentials
+auth = tweepy.OAuthHandler('XAjruRZe6O6gen4zGLJL5VTbw', 'vw3a8tb1AnMXEEUI3HXt7Xq5CpvjB0LNEtmAf5PmJCZYkQ3RPq')
+auth.set_access_token('906982976344481794-7Nbvj6fCqkcTHNVoaepvq6iIMIJxc8G', 'sDFWRJDAcscgFkGf0Sp1M4iPB0K5FLMwW6ck2y3ANMy9a')
+
+  # Construct the API instance
+api = tweepy.API(auth)
+
 #Watson Login Credentials
 natural_language_understanding = NaturalLanguageUnderstandingV1(
   username= "3ca320cb-0473-4ae1-9d63-a72d139c27c2",
   password= "tLrq6dXUA1D5",
   version='2017-02-27')
+
+def getInfo():
+    print(api.user_timeline([barrirafferty][0]))
+
+getInfo()
 
 def nluRun(txt):
     response = natural_language_understanding.analyze(
