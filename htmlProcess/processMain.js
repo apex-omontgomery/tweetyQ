e29f773a0953ffc306d37a1ac4047995a0e86695
wimo7083 <wimo7083@colorado.edu>
2018-02-24 18:34:23 -0700

    its alive

--- htmlProcess/processMain.js
+++ htmlProcess/processMain.js
@@ -2,46 +2,51 @@
 const getAllTweetDoms = () => {
   let users = [];
   const allTweetElements = document.getElementsByClassName('tweet');
-  const builtTweetElements = allTweetElemnts.map(
-    singleTweet => tweetObjectBuilder
-  );
+
+  return Array.from(allTweetElements).map(singleTweet => {
+    return tweetObjectBuilder(singleTweet);
+  });
 };
 
 // get url, get text, get tweet id, get user id
 const tweetObjectBuilder = singleTweet => {
   const tweetObject = singleTweet.getElementsByClassName(
     'js-tweet-text-container'
-  );
+  )[0];
+  const tweetText = tweetObject.getElementsByClassName('tweet-text');
+  const urlObj = tweetObject.getElementsByClassName('twitter-timeline-link');
 
   return {
     userId: singleTweet.getAttribute('data-user-id'),
     tweetId: singleTweet.getAttribute('data-item-id'),
-    text: singleLevelText(tweetObject),
-    url: getSingleUrl(tweetObject)
+    text: singleLevelTweetText(tweetText),
+    url: getSingleUrl(urlObj),
+    isArticle: isTweetArticle(tweetObject)
   };
 };
 
-const getSingleUrl = jsTweetContainer => {
-  let returnUrl = '';
-  const url = jsTweetContainer.getElementsByClassName('twitter-timeline-link');
-  if (url.length > 0) {
-    returnUrl = url[0].href;
-  }
+const getSingleUrl = tweetObject => {
+  const lastLink = Array.from(tweetObject).pop(); //convert to array,get last item
+  return typeof lastLink === undefined ? '' : lastLink.href;
 };
 
-const singleLevelTweetText = jsTweetContainer => {
-  (child = jsTweetContainer.firstChild), (texts = []);
-
+const singleLevelTweetText = textObject => {
+  (child = textObject[0].firstChild), (texts = []);
+  console.log(child);
   while (child) {
     if (child.nodeType == 3) {
+      console.log('child found');
+      console.log(child);
       texts.push(child.data);
     }
-    return child.nextSibling;
+    child = child.nextSibling;
   }
   return texts.join('');
 };
 
 // returns if tweet is article or non-article
-const classifyTweetType = tweetDomItem => {
+const isTweetArticle = tweetDomItem => {
   return 0;
 };
+
+console.log(getAllTweetDoms());
