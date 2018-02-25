ba6cbb8cc30862a769d344bf8baf103d4122b4e2
wimo7083 <wimo7083@colorado.edu>
2018-02-25 03:49:08 -0700

    snakes

--- py_back/py_back.py
+++ py_back/py_back.py
@@ -1,15 +1,28 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from twitter_interact import get_tweets
+
 
 app = Flask(__name__)
 
 
-@app.route('/user')
+@app.route('/tweet')
 def user_analysis():
+    json_data = request.get_json()
     return 'Hello World!'
 
-@app.route('/tweet')
+@app.route('/user', methods=['GET', 'POST'])
 def tweet_analysis():
-    return 'Hello World!'
+    json_data = request.get_json()
+
+    if not json_data:
+        json_data['personality_data'] = get_tweets('27260086')
+        return jsonify(json_data)
+        return jsonify({'ok': False, 'error': 'Missing Required Body Params'}), 400
+    # json_data['personality_data'] = get_tweets(json_data['user_list'])
+    vals = json_data['personality_data'] = get_tweets('27260086')
+
+    return jsonify(vals), 200
+
 
 if __name__ == '__main__':
     app.run()
---
+++ py_back/twitter_interact.py
@@ -0,0 +1,20 @@
+import tweepy
+
+
+#Twitter API credentials
+consumer_key = 'lItoXUlw5ZyQupYN0xKXwYGrc'
+consumer_secret = 'G0m6to7CxMWwTBI3K4DFeb9PMtaKLOTzFt8ASxuLTUK6ZA6xET'
+access_key = 	'941729945411231744-zJK9hYIReUlyjr1LIuBMnv97UA0PKmp'
+access_secret = 'UXE4Cv9T6qFbiUDRgmrZEvW0ojCJ92ck6aXzhWXeZUFxA'
+
+def get_tweets(user_id: str)-> str:
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_key, access_secret)
+    api = tweepy.API(auth)
+    all_tweets = []
+
+    for tweet in api.user_timeline(user_id=user_id, count=150):
+        if not tweet.retweeted:
+            all_tweets.append(tweet.text)
+
+    return ''.join(all_tweets)
