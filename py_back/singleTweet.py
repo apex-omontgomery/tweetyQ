1b35b5a86b8f0fb7052c03ec5583d6918566930e
aidan <32107433+arafferty10@users.noreply.github.com>
2018-02-25 05:15:55 -0700

    nlu instead of ta

--- py_back/singleTweet.py
+++ py_back/singleTweet.py
@@ -1,25 +1,27 @@
-print " "
-print "The bot is coming to life..."
-print " "
-
 import json
-import tweepy
-from watson_developer_cloud import ToneAnalyzerV3
-
-#Twitter Login Credentials
-auth = tweepy.OAuthHandler('XAjruRZe6O6gen4zGLJL5VTbw', 'vw3a8tb1AnMXEEUI3HXt7Xq5CpvjB0LNEtmAf5PmJCZYkQ3RPq')
-auth.set_access_token('906982976344481794-7Nbvj6fCqkcTHNVoaepvq6iIMIJxc8G', 'sDFWRJDAcscgFkGf0Sp1M4iPB0K5FLMwW6ck2y3ANMy9a')
+from watson_developer_cloud import NaturalLanguageUnderstandingV1
+from watson_developer_cloud.natural_language_understanding_v1 \
+  import Features, EntitiesOptions, KeywordsOptions
 
 #Watson Login Credentials
-tone_analyzer = ToneAnalyzerV3(
-  version='2017-09-21',
-  username='a6f558d8-06d2-45a7-93a7-5c358290b6a4',
-  password='AaRIysTakCa6'
-)
+natural_language_understanding = NaturalLanguageUnderstandingV1(
+  username= "3ca320cb-0473-4ae1-9d63-a72d139c27c2",
+  password= "tLrq6dXUA1D5",
+  version='2017-02-27')
 
-def toneAnalyze(tweet):
-    print(json.dumps(tone_analyzer.tone(tone_input= tweet,
-                                    content_type="text/plain"), indent=2))
+def nluRun(txt):
+    response = natural_language_understanding.analyze(
+      text= txt,
+      features=Features(
+        entities=EntitiesOptions(
+          emotion=True,
+          sentiment=True,
+          limit=3),
+        keywords=KeywordsOptions(
+          emotion=True,
+          sentiment=True,
+          limit=3)))
+    print(json.dumps(response, indent=2))
 
-tweetPhrase = 'I am very happy. It is a good day.'
-toneAnalyze(tweetPhrase)
+tweet = "I'm like hey what's up hello"
+nluRun(tweet)
